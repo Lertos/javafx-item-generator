7f97fe097dc18d23dc8328cefaa50c88883ed18e
Lertos <deetasticgaming@gmail.com>
2023-01-10 17:45:03 -0400

    Changing default names

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.lertos.javafxitemgenerator {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.lertos.javafxitemgenerator to javafx.fxml;
     exports com.lertos.javafxitemgenerator;
 }
