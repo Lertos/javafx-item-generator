ee699e0cd26dcb87e19ef2a2191fd8acf8db6550
Lertos <deetasticgaming@gmail.com>
2023-01-11 10:52:03 -0400

    Adding the java.sql dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.lertos.javafxitemgenerator {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
 
     opens com.lertos.javafxitemgenerator to javafx.fxml;
     exports com.lertos.javafxitemgenerator;
