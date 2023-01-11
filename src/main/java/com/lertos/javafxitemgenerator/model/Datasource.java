83554770786b1350e660ce9c168645b94afbff6d
Lertos <deetasticgaming@gmail.com>
2023-01-11 16:36:46 -0400

    Adding the initial table creation for items

--- src/main/java/com/lertos/javafxitemgenerator/model/Datasource.java
+++ src/main/java/com/lertos/javafxitemgenerator/model/Datasource.java
@@ -2,12 +2,31 @@ package com.lertos.javafxitemgenerator.model;
 
 import java.io.File;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Datasource {
 
     private final String DB_NAME = "items.db";
     private final File temp = new File(DB_NAME);
     private final String CONNECTION_STRING = "jdbc:sqlite:" + temp.getAbsolutePath().replace("\\","\\\\");
+
+    private final String CREATE_ITEMS_TABLE = "CREATE TABLE IF NOT EXISTS items (" +
+            "id INT PRIMARY KEY," +
+            "name TEXT NOT NULL," +
+            "rarity TEXT NOT NULL," +
+            "type TEXT NOT NULL," +
+            "buy_price INT NOT NULL," +
+            "sell_price INT NOT NULL," +
+            "description TEXT NULL," +
+            "class_req TEXT NULL," +
+            "level_req INT NULL," +
+            "one_handed INT NULL," +
+            "equip_slot TEXT NULL," +
+            "dmg_min INT NULL," +
+            "dmg_max INT NULL," +
+            "armor INT NULL," +
+            "health INT NULL" +
+            ");";
 
     public static final String TABLE_ITEMS = "items";
 
@@ -44,6 +63,9 @@ public class Datasource {
         try {
             conn = DriverManager.getConnection(CONNECTION_STRING);
 
+            //Set up the initial state of the database
+            createTables();
+
             return true;
         } catch (SQLException e) {
             System.out.println("Couldn't connect to database: " + e.getMessage());
@@ -60,4 +82,11 @@ public class Datasource {
         }
     }
 
+    private void createTables() {
+        try (Statement statement = conn.createStatement()) {
+            statement.executeQuery(CREATE_ITEMS_TABLE);
+        } catch (SQLException e) {
+            System.out.println("Query failed: " + e.getMessage());
+        }
+    }
 }
