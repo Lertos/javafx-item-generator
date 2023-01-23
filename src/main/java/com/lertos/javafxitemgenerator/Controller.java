6f9185f7bb2e72ddf852e102246b036591567d72
Lertos <deetasticgaming@gmail.com>
2023-01-22 21:05:18 -0400

    Removing fields and simplifying

--- src/main/java/com/lertos/javafxitemgenerator/Controller.java
+++ src/main/java/com/lertos/javafxitemgenerator/Controller.java
@@ -1,23 +1,29 @@
 package com.lertos.javafxitemgenerator;
 
 import com.lertos.javafxitemgenerator.model.SetupData;
+import com.lertos.javafxitemgenerator.model.enums.ItemTypes;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class Controller {
 
     @FXML
+    private TextField tfItemId;
+    @FXML
+    private TextField tfItemName;
+    @FXML
+    private TextField tfLevelReqWeapon;
+    @FXML
+    private TextField tfDmgMin;
+    @FXML
+    private TextField tfDmgMax;
+    @FXML
     private ChoiceBox cbItemTypes;
     @FXML
-    private ChoiceBox cbRarities;
-    @FXML
     private ChoiceBox cbWeaponClasses;
-    @FXML
-    private ChoiceBox cbArmorClasses;
-    @FXML
-    private ChoiceBox cbEquipmentSlots;
     @FXML
     private Button btnSave;
     @FXML
@@ -29,16 +35,10 @@ public class Controller {
         SetupData setupData = new SetupData();
 
         cbItemTypes.getItems().addAll(setupData.getItemTypes());
-        cbRarities.getItems().addAll(setupData.getRarities());
         cbWeaponClasses.getItems().addAll(setupData.getClasses());
-        cbArmorClasses.getItems().addAll(setupData.getClasses());
-        cbEquipmentSlots.getItems().addAll(setupData.getEquipmentSlots());
 
         cbItemTypes.getSelectionModel().select(0);
-        cbRarities.getSelectionModel().select(0);
         cbWeaponClasses.getSelectionModel().select(0);
-        cbArmorClasses.getSelectionModel().select(0);
-        cbEquipmentSlots.getSelectionModel().select(0);
     }
 
     @FXML
